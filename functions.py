152517a7aea1ff93f80cf4dfea203e6c7928aa11
ravindra kumar saini <raveensbsaini@gmail.com>
2024-11-13 16:39:16 +0530

    daily commit

--- functions.py
+++ functions.py
@@ -1,18 +1,60 @@
 import subprocess,shlex
 import re
+# def print(*args,**kwargs):
+#     return None
+    
 def find_event():
+    return_list = []
     with open("/proc/bus/input/devices","r") as file:
         file_data = file.read()
         file_data = file_data.split("\n\n")
+        EV_SYN= None #(0x00): Synchronization events
+        EV_KEY= None #(0x01): Keyboard/Button events
+        EV_REL= None #(0x02): Relative axis events (like mouse movement)
+        EV_ABS= None #(0x03): Absolute axis events (like touchscreen)
+        EV_MSC= None #(0x04): Miscellaneous events
+        EV_LED= None #(0x11): LED events
+        EV_SND= None #(0x12): Sound events
+        mouse = None
         for row in file_data:
             print("-------------------------------------------------------")
-            print(row)
-            new = re.match("H:",row,re.DOTALL)
-            print("new =======",new)
-            if new != None:
-                print(new[0])
-                break
+            match_kbd = re.search("Handlers=.*?kbd.*",row)
+            match_Handlers = re.search("Handlers=.*",row)
+            match_mouse = re.search("Handlers=.*?mouse.*",row)
+            match_ev = re.search("EV=(.*)",row)
+            match_name = re.search('N: Name=(.*)',row)           
+            match_eventx = re.search("event(\d+)",match_Handlers[0])
+            eventx = int(match_eventx[1])
+            print("eventx = ",eventx)
+            if match_ev != None:
+                match_ev =int(match_ev[1],16)
+                match_ev = bin(match_ev)[2:].zfill(16)
+                print(match_ev)
+                if match_ev[-1] == "1":
+                    EV_SYN=True 
+                if match_ev[-2] == "1":
+                    EV_KEY = True
+                if match_ev[-3] == "1":
+                    EV_REL= True
+                if match_ev[-4] == "1":
+                    EV_ABS = True
+                if match_ev[-5] == "1":
+                    EV_MSC = True
+                if match_ev[-6] == "1":
+                    EV_LED = True
+                if match_ev[-7] == "1":
+                    EV_SND = True          
+        
+            if match_kbd != None and  EV_KEY == True:
+                print("keyboard events matches")
+                return_list.append((match_name[1][1:-1],eventx))
+            elif match_mouse != None and EV_REL == True:
+                return_list.append((match_name[1][1:-1],eventx))
+
+            if match_mouse != None and EV_REL == True:
+                print("moue event matches")
+            print(return_list)
+    return return_list
 if __name__ == "__main__":
     find_event()
-        
     
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import _nameToLevel
 import struct    
 import time
 import subprocess,shlex
@@ -7,44 +8,28 @@ import sys
 from databases import Database
 import functions
 database = Database('sqlite+aiosqlite:///database.db')
-eventx = functions.find_event()
-
-async def keyboard():
-    global keypress_data
-    print("keyboard function start")
-
-    filepath = f"/dev/input/{eventx}"
-        
+list_of_events = functions.find_event()
+print(list_of_events)
+  
+async def input(name,eventx):
+    print("listening to event",eventx)
+    filepath = f"/dev/input/event{eventx}"
     event_format = "llHHI"
-
     bytes_size = struct.calcsize(event_format)
-    print(bytes_size)
     count = 0
+
     async with aiofiles.open(filepath,"rb") as file:
         while True:
-            print("in while loop")
             byte_array  = await file.read(bytes_size)
-
             if byte_array:
                 (tv_sec, tv_usec, ev_type, ev_code, ev_value) = struct.unpack(event_format, byte_array) 
-                print(f"{tv_sec=}, {tv_usec=}")
                 timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(tv_sec)) + f".{tv_usec:06d}"
                 print(f"Time: {timestamp}, Type: {ev_type}, Code: {ev_code}, Value: {ev_value}")
-                if ev_type == 1:
-                    pass
-                    # print(ev_type,"keypress")
-                    # async with database.transaction():
-                    #     query = "INSERT INTO  keypress(event_key,event_value,time) as (:event_key,:event_value,timestamp)"
-                    #     values = {"event_key":ev_code,"event_value":ev_value,"time":time.time()}
-                    #     await database.execute(query=query,values=values)   
-                    # keypress 
-
-                if ev_type == 2:
-                    pass
-                    # mousepress 
-                if ev_type == 3:
-                    pass
-                    # touchscree
+                if ev_type == 1: # I am bit sure does i still check it
+                        async with database.transaction():
+                            query = "INSERT INTO  keypress(name,event_key,event_value,time) as (:event_key,:event_value,timestamp)"
+                            values = {"event_key":ev_code,"event_value":ev_value,"time":time.time()}
+                            await database.execute(query=query,values=values)    
             else:
                 print("========== didn't got byte_array")
                 break
@@ -60,5 +45,5 @@ async def window():
             query="insert into windows_title(title,)"
         await asyncio.sleep(10)
 async def main():
-    await asyncio.gather(keyboard())
+    await asyncio.gather(*[input(name,eventx) for name,eventx in list_of_events ])
 asyncio.run(main())
